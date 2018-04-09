70158f5eed53066a0164f2df00b698c5e96c554d
5fi26 <yc34493@uga.edu>
2018-04-09 16:33:48 -0400

    Can use options -c, -l, -w, but will not work for just '-'

--- wc.cpp
+++ wc.cpp
@@ -1,13 +1,12 @@
+
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <fcntl.h>
 #include <unistd.h>
+#include <getopt.h>
 
 using namespace std;
-
-
-
 
 int main(int argc, char** argv){
   int fd;
@@ -20,10 +19,36 @@ int main(int argc, char** argv){
   int totalLines = 0;
   int totalWords = 0;
   int totalBytes = 0;
-  for(int i = 1; argv[i] != NULL; i++){ 
+  bool isC = false; 
+  bool isL = false;
+  bool isW = false;
+
+    int c;
+    while((c = getopt(argc, argv, "l:c:w:")) != -1){
+      switch (c){ 
+      case 'l': 
+	isL = true;
+	break;
+
+      case 'c': 
+	isC = true;
+	break;
+	
+      case 'w': 
+	isW = true;
+	break;
+      }
+    }//while
+
+
+  for(int i = 1; argv[i] != NULL; i++){  //Reads all the files and computes the lines, bytes and words       
+    if(*argv[i] == '-'){
+      continue;
+    }
+    
     fd = open(argv[i], O_RDONLY);
     // check if fd is good                                                                                                                           
-    if(fd < 0) cout << "error opening " << argv[1] << endl;     
+      if(fd < 0) cout << "error opening " << argv[i] << endl;     
     //Computes bytes, newLines and spaces
       while((n = read(fd, buffer, 1)) > 0){
 	byteCount++;
@@ -40,19 +65,51 @@ int main(int argc, char** argv){
 	  inSpace = false;
 	}
       }//while
-      cout << "New Line Counts:" <<  newLineCount << endl;
+        
       totalLines += newLineCount;
+      totalWords += wordCount;
+      totalBytes += byteCount;
+     
+      if(isC == true){
+	cout << byteCount << endl;
+	byteCount = 0;
+	
+      }
+      
+      if(isL){
+	cout << newLineCount << endl;
+	newLineCount = 0;
+      }
+      
+      if(isW){
+	cout << wordCount << endl;
+	wordCount = 0;
+      }
+
+      else{
+      cout << "New Line Counts:" <<  newLineCount << endl;
       newLineCount = 0;
       cout << "Word counts:" << wordCount<< endl;
-      totalWords += wordCount;
       wordCount = 0;
       cout << "Byte Counts: " << byteCount << endl;
-      totalBytes += byteCount; 
       byteCount = 0;
       cout << argv[i] << endl;
-  }//for                          
-  cout << "L W B: " << totalLines << " " << totalWords << " " << totalBytes << " total" << endl;
-}
+      }//else
+  }//for
+      if(isL){
+	cout << totalLines << " total" << endl;
+      }
+      if(isC){
+	cout << totalBytes << " total" << endl;
+      }
+      if(isW){
+	cout << totalWords << " total" << endl;
+      }
+      else {
+	cout << "L W B: " << totalLines << " " << totalWords << " " << totalBytes << " total" << endl;
+      }
+      
+  }//main
 
 
 
